1b74cf346da20688a6b8d70b3698cc2ea7f032e5
FaiziIsmady <faiziismady5@gmail.com>
2024-08-28 22:31:03 +0700

    Menambahkan URL deployment PWS

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o4cvjnqsxi29%*-u%ckc$^43si*25*87u=@y%oy5ca$=9+!e7t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "muhammad-faizi-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definition
