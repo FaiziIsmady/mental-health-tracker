edb716e4a7d9eb712f409765edd21073abb62d26
FaiziIsmady <faiziismady5@gmail.com>
2024-09-11 14:01:41 +0700

    Menambahkan file Tutorial 2

--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
+import uuid  # tambahkan baris ini di paling atas
 from django.db import models
 
 class MoodEntry(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     mood = models.CharField(max_length=255)
     time = models.DateField(auto_now_add=True)
     feelings = models.TextField()
